e4f55531fb9d6ba6a0280a5be4975ef458b0906c
Peter Benzoni <pbenzoni@gmfus.org>
2023-03-17 09:38:15 -0400

    Adding object guard rails

--- objects/q_and_a.py
+++ objects/q_and_a.py
@@ -13,3 +13,19 @@ class Q_and_A(Base):
     question = Column(String)
     answer = Column(String)
     votes = Column(Integer)
+
+    @classmethod
+    def get_allowed_keys(cls):
+        # Get the column names from the table metadata
+        column_names = cls.__table__.columns.keys()
+        # Convert the column names to a list of strings
+        allowed_keys = [str(column) for column in column_names]
+        return allowed_keys
+
+    def __init__(self, **kwargs):
+        # Get the allowed keys for this class
+        allowed_keys = self.get_allowed_keys()
+        # Filter out any keys that are not in the allowed_keys list
+        valid_kwargs = {k: v for k, v in kwargs.items() if k in allowed_keys}
+        # Use the filtered kwargs to initialize the object attributes
+        self.__dict__.update(valid_kwargs)
--- objects/visual_story.py
+++ objects/visual_story.py
@@ -14,3 +14,19 @@ class Visual_Story(Base):
     thumbnail = Column(String)
     source = Column(String)
     source_icon = Column(String)
+        
+    @classmethod
+    def get_allowed_keys(cls):
+        # Get the column names from the table metadata
+        column_names = cls.__table__.columns.keys()
+        # Convert the column names to a list of strings
+        allowed_keys = [str(column) for column in column_names]
+        return allowed_keys
+
+    def __init__(self, **kwargs):
+        # Get the allowed keys for this class
+        allowed_keys = self.get_allowed_keys()
+        # Filter out any keys that are not in the allowed_keys list
+        valid_kwargs = {k: v for k, v in kwargs.items() if k in allowed_keys}
+        # Use the filtered kwargs to initialize the object attributes
+        self.__dict__.update(valid_kwargs)
