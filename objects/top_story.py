adfc197cf39bcab93047f9d10a9109f46e3946fd
Peter Benzoni <pbenzoni@gmfus.org>
2024-05-15 17:30:44 -0400

    adding objects 2

--- objects/top_story.py
+++ objects/top_story.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, JSON, Text
+from sqlalchemy import Column, Integer, String, JSON, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 
